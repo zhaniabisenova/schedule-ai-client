7838dda8a0a18593b1440ba8bb97493f79099203
zhaniya <zhaniabisenova@gmail.com>
2025-10-08 15:57:48 +0500

    privet sdfsdfsdf

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -7,6 +7,7 @@ Profile Page
 <button onClick={()=>{navigate('/login')} }>Логин </button>
 <button onClick={()=>{navigate('/profile')} }>Профиль </button>
 <button onClick={()=>{navigate('/admin')} }>Админ </button>
-<button onClick={()=>{navigate('/')} }>Главная </button>
+<button onClick={()=>{navigate('/')} }>Главная </button>gfnfgngfngfnfb
+fbfxbf
 </>)
 }
