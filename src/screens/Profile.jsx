bd7c23f444e46a32c7840994b7229834a1487253
zhaniya <zhaniabisenova@gmail.com>
2025-10-08 16:10:02 +0500

    clclcl

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -7,7 +7,7 @@ Profile Page
 <button onClick={()=>{navigate('/login')} }>Логин </button>
 <button onClick={()=>{navigate('/profile')} }>Профиль </button>
 <button onClick={()=>{navigate('/admin')} }>Админ </button>
-<button onClick={()=>{navigate('/')} }>Главная </button>gfnfgngfngfnfb
+<button onClick={()=>{navigate('/')} }>Главная </button>gfnfgngfngfnfbhtrhtrhr trhtrhtrhtrh
 fbfxbf
 </>)
 }
